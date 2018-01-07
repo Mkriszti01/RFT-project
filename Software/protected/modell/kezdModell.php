e13ba4480a3cb34eb03bc0257585eadf5e3ff183
Mkriszti01 <34301049+Mkriszti01@users.noreply.github.com>
2018-01-07 16:00:50 +0100

    Update kezdModell.php

--- Software/protected/modell/kezdModell.php
+++ Software/protected/modell/kezdModell.php
@@ -5,20 +5,22 @@ class kezdModell {
 	function kezd(){
 		
 		return "<h1>Üdvözöljük a Fanatikvárium oldalán!</h1>".
-"<p>
-Családi vállalkozásunk több mint 15 áll az antik könyvek kedvelőinek szolgálatában. Elindításakor egy álom vált valóra. Az alapító már gyermek korában arról álmodozott, hogy egyszer  könyveket restaurál és azokat saját antikváriumának polcain teszi elérhetővé a műértők számára. Ezzel az álommal nyitottunk meg 2010 januárjában, 1500 kötettel Eger szívében, a Dobó téren. 
+"<p>Családi vállalkozásunk több mint 15 éve áll az antik könyvek kedvelőinek szolgálatában. Elindításakor egy álom vált valóra. Az alapító már gyermek korában arról álmodozott, hogy egyszer  könyveket restaurál és azokat saját antikváriumának polcain teszi elérhetővé a műértők számára. Ezzel az álommal nyitottunk meg 2010 januárjában, 1500 kötettel Eger szívében, a Dobó téren. Könyveink száma mára meghallatta az 5000-es darabszámot, köztök több ritkasággal is. 
 </p><p>
-Működésünk során kínálatunk folyamatosan változik. Árukészletünk akár több száz kötettel bővülhet naponta, melyet nagy gonddal dolgozunk fel és szükség esetén a restaurátori munkákat is elvégezzük. 
+Működésünk során kínálatunk folyamatosan változik. Árukészletünk akár több száz kötettel bővülhet naponta, melyet nagy gonddal dolgozunk fel és szükség esetén a restaurátori munkákat is elvégezzük. C
 </p><p>
-Ide még lesz szöveg
+Internetes oldalunkkal arra vállalkozunk, hogy a világhálón is áttekinthesse a széles közönség aktuális ajánlatainkat. Ezzel azt szeretnénk elérni, hogy mindenki rátaláljon a legjobb könyvekre, és a legjobb könyvek is rátaláljanak olvasóikra
+</p><p>
+Nézzen szét nálunk, válogasson széleskörű kínálatunk közt. Ha mégsem találja a könyvet, amit régóta keres vegye fel velünk a kapcsolatot és mi segítünk felkutatni, 
 </p><p>
 “Szeretem a könyvet, mert holt betűiben benne van minden, ami elmúlt, és minden, ami lesz.”
 </p><p>
 Fekete István
 </p><p>
+</p><p>
 Barátsággal,
 </p><p>
-a Fanatikvárium csapata</p>	" ;
+a Fanatikvárium csapata</p>	"  ;
 		
 	}
 	
