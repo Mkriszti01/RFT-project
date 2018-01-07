a7642b84816138d70fe226a672a0612294d5443f
Mkriszti01 <34301049+Mkriszti01@users.noreply.github.com>
2018-01-07 15:27:33 +0100

    Update kezdModell.php

--- Software/protected/modell/kezdModell.php
+++ Software/protected/modell/kezdModell.php
@@ -5,11 +5,16 @@ class kezdModell {
 	function kezd(){
 		
 		return "<h1>Üdvözöljük a Fanatikvárium oldalán!</h1>".
-"<p>Családi vállalkozásunk több mint két évtizede áll a népzene kedvelőinek és művelőinek szolgálatában. Elindításakor az volt a szándékunk, hogy fórumot teremtsünk a Kárpát-medence népzenei hagyományának átfogó megismeréséhez. Igyekszünk a lehető legtöbb hangszerkészítővel kapcsolatot teremteni és tartani, érdekességeikből vásárlóinknak izgalmas kínálatot nyújtani határainkon innen és túlról. Hangszerkészletünk a legszélesebb skálán foglalja magába a klasszikus gyári és kézi készítésű zenei eszközöket. Célkitűzésünket nagymértékben segítette a zenei életben eltöltött több évtized, és hogy a család minden tagjának zenei végzettsége van. Így hangszereink nem pusztán kipróbálhatóak, de be is tudjuk mutatni őket, akár gyakorlati tanácsokat is adva.
+"<p>
+Családi vállalkozásunk több mint 15 áll az antik könyvek kedvelőinek szolgálatában. Elindításakor egy álom vált valóra. Az alapító már gyermek korában arról álmodozott, hogy egyszer  könyveket restaurál és azokat saját antikváriumának polcain teszi elérhetővé a műértők számára. Ezzel az álommal nyitottunk meg 2010 januárjában, 1500 kötettel Eger szívében, a Dobó téren. 
 </p><p>
-Működésünk során kínálatunkat bővíteni is tudtuk, így az mára nem csak a Kárpát-medence hangszereit öleli fel, de felvonultatja a világ számos kultúrájának hangszereit is. A hangszerek mellett természetesen alkatrészeket és kiegészítőket is forgalmazunk, továbbá gazdag népzenei kotta és CD választék várja kedves vevőinket.
+Működésünk során kínálatunk folyamatosan változik. Árukészletünk akár több száz kötettel bővülhet naponta, melyet nagy gonddal dolgozunk fel és szükség esetén a restaurátori munkákat is elvégezzük. 
 </p><p>
-Internetes oldalunkkal arra vállalkozunk, hogy a világhálón is hozzáférhetővé tegyük boltunk készletének egy részét, megkönnyítve a tájékozódást és a vásárlást. Webáruházunkban szereplő termékeinket a lehető legteljesebb módon szeretnénk bemutatni Önöknek, hogy igény szerint a bolt meglátogatása nélkül is lehessen azok között válogatni, vásárolni. Természetesen nehéz egy hangszert megtekintés és kipróbálás nélkül megvenni. Bízunk benne, hogy szaktudásunkkal és tapasztaltunkkal, felmerülő kérdéseikre adott pontos válaszainkkal át tudjuk hidalni ezt a nehézséget!
+Ide még lesz szöveg
+</p><p>
+“Szeretem a könyvet, mert holt betűiben benne van minden, ami elmúlt, és minden, ami lesz.”
+</p><p>
+Fekete István
 </p><p>
 Barátsággal,
 </p><p>
